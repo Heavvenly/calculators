762444470e5462e14da65c24b34c61712aa1dac2
Heavvenly <123119089+Heavvenly@users.noreply.github.com>
2023-01-19 13:44:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+var curnum;
+var value;
+var numlist = [];
+var mean = 0;
+var sd = 0;
+
+window.onload = function() {
+  value = '';
+  document.getElementById('curnum').innerHTML = "Type Number";
+}
+
+function anum(choice) {
+
+  curnum = choice;
+  value += curnum;
+  document.getElementById('curnum').innerHTML = value;
+
+
+
+
+}
+
+function add() {
+  numlist.push(parseInt(value))
+  value = '';
+
+  document.getElementById('curnum').innerHTML = "‎ ‎ ‎ ";
+
+}
+
+function equals() {
+  numlist.push(parseInt(value));
+
+  if(value!=undefined){
+    document.getElementById('curnum').innerHTML = value;
+  }
+  else{
+    document.getElementById('curnum').innerHTML = "‎ ‎ ";
+  }
+
+
+
+
+  for (let i = 0; i < numlist.length; i++) {
+      mean = mean + parseInt(numlist[i])
+    }
+    mean = parseFloat(mean / numlist.length)
+
+    //alert("Average: " + mean)
+
+  for (let i = 0; i < numlist.length; i++) {
+    sd += (Math.pow(Math.abs(numlist[i] - mean) , 2))
+  }
+
+    sd /= numlist.length
+    sd = Math.sqrt(sd)
+
+  //alert("Standard Deviation: " + parseFloat(sd))
+
+  document.getElementById('curnum').innerHTML = "‎Average/Mean: "+mean;
+  document.getElementById('sdtext').innerHTML = "Standard Deviation: " + sd;
+  document.getElementById('curtex').innerHTML = "Your Results:";
+
+}
+
+function reset(){
+  curnum = 0;
+  value = '';
+  numlist = [];
+  mean = 0;
+  sd = 0;
+  document.getElementById('curnum').innerHTML = "Type Number";
+
+}
+
+function seeset(){
+  alert("Your set currently has: "+numlist)
+}
+
+
+
+
+
+
+
+
+function boranum() {
+  var borcurnum = parseInt(prompt("enter number"));
+  numlist.push(parseInt(borcurnum))
+}
